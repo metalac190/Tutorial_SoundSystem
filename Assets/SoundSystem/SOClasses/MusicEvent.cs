e92883cf8dc20a0d02b1d0441a9052b1e78bc5bb
Adam <achaysmail@gmail.com>
2021-04-22 09:31:14 -0500

    Imported Audio Assets
    Created and imported music and sound files

---
+++ Assets/SoundSystem/SOClasses/MusicEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public enum LayerType
+{
+    Additive,
+    Single
+}
+
+[CreateAssetMenu(menuName = "SoundSystem/Music Event", fileName = "MUS_")]
+public class MusicEvent : ScriptableObject
+{
+    // optionally, add volume
+    [SerializeField] AudioClip[] _musicLayers;
+    [SerializeField] LayerType _layerType = LayerType.Additive;
+    [SerializeField] AudioMixerGroup _mixer;
+
+    public AudioClip[] MusicLayers => _musicLayers;
+    public LayerType LayerType => _layerType;
+    public AudioMixerGroup Mixer => _mixer;
+}
