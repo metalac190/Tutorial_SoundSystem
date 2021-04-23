13a7a84fd2cfeaa9a2b71f0a43485efaaeb01c4d
Adam <achaysmail@gmail.com>
2021-04-23 09:25:59 -0500

    Configuring Sound Assets
    Configured sound assets with music clips, blend type and mixer support

--- Assets/SoundSystem/SOClasses/MusicEvent.cs
+++ Assets/SoundSystem/SOClasses/MusicEvent.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public enum LayerType
+namespace SoundSystem
 {
-    Additive,
-    Single
+    public enum LayerType
+    {
+        Additive,
+        Single
+    }
+
+    [CreateAssetMenu(menuName = "SoundSystem/Music Event", fileName = "MUS_")]
+    public class MusicEvent : ScriptableObject
+    {
+        // optionally, add volume
+        [SerializeField] AudioClip[] _musicLayers;
+        [SerializeField] LayerType _layerType = LayerType.Additive;
+        [SerializeField] AudioMixerGroup _mixer;
+
+        public AudioClip[] MusicLayers => _musicLayers;
+        public LayerType LayerType => _layerType;
+        public AudioMixerGroup Mixer => _mixer;
+    }
 }
 
-[CreateAssetMenu(menuName = "SoundSystem/Music Event", fileName = "MUS_")]
-public class MusicEvent : ScriptableObject
-{
-    // optionally, add volume
-    [SerializeField] AudioClip[] _musicLayers;
-    [SerializeField] LayerType _layerType = LayerType.Additive;
-    [SerializeField] AudioMixerGroup _mixer;
-
-    public AudioClip[] MusicLayers => _musicLayers;
-    public LayerType LayerType => _layerType;
-    public AudioMixerGroup Mixer => _mixer;
-}
